753830a700d19caf4e7510ec7a4ff33989c7c483
david <davidvalenciaredrovan@gmail.com>
2022-09-27 18:03:50 +1300

    add openAI test file

--- VAE.py
+++ VAE.py
@@ -1,1 +1,16 @@
-Python file
+import gym
+
+env = gym.make("BreakoutNoFrameskip-v4")
+
+print("Observation Space: ", env.observation_space)
+print("Action Space       ", env.action_space)
+
+
+obs = env.reset()
+
+for i in range(1000):
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    env.render()
+    time.sleep(0.01)
+env.close()
---
+++ openAI_env_test.py
@@ -0,0 +1,28 @@
+import gym
+import ale_py
+
+print('gym:', gym.__version__)  # gym: 0.21.0
+print('ale_py:', ale_py.__version__)  # ale_py: 0.7.1
+
+#env = gym.make('CartPole-v1')
+#env = gym.make('CarRacing-v0')  # pip3 install Box2D gym
+env = gym.make('Pendulum-v1')
+#env = gym.make("LunarLander-v2")
+#env = gym.make("Breakout-v0")
+#env = gym.make("BipedalWalker-v3")
+
+
+for e in range(20):
+
+    obser = env.reset()
+
+    for t in range(10000):
+
+        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info, = env.step(action)
+        if done:
+            print(f"Episode {e + 1} finished after {t + 1} timesteps")
+            break
+
+
